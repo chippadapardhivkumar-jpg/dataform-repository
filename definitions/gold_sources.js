a888de835389f164674bb94928c9a58bad91629c
Pardhiv kumar Chippada <chippadapardhivkumar@gmail.com>
2026-07-20 17:32:24 +0000

    implementation of bronze, silver and gold layer

---
+++ definitions/gold_sources.js
@@ -0,0 +1,23 @@
+declare({
+  database: "project-c8e56921-a2a7-43dc-894",
+  schema: "retail_gold",
+  name: "dim_customer"
+});
+
+declare({
+  database: "project-c8e56921-a2a7-43dc-894",
+  schema: "retail_gold",
+  name: "dim_product"
+});
+
+declare({
+  database: "project-c8e56921-a2a7-43dc-894",
+  schema: "retail_gold",
+  name: "dim_store"
+});
+
+declare({
+  database: "project-c8e56921-a2a7-43dc-894",
+  schema: "retail_gold",
+  name: "fact_sales"
+});
---
+++ definitions/sources.js
@@ -0,0 +1,11 @@
+declare({
+    database: "project-c8e56921-a2a7-43dc-894",
+    schema: "retail_bronze",
+    name: "raw_pos_transactions"
+});
+
+declare({
+    database: "project-c8e56921-a2a7-43dc-894",
+    schema: "retail_bronze",
+    name: "raw_pos_transactions_incremental"
+});
